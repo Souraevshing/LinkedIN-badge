792e142c22c4be056534ee8a48e9d32e0e3f8ddc
Sourav Kumar <isauravshing@gmail.com>
2022-07-24 15:45:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div
+      class="badge-base LI-profile-badge"
+      data-locale="en_US"
+      data-size="large"
+      data-theme="dark"
+      data-type="HORIZONTAL"
+      data-vanity="sourav-kumar-823b7415b"
+      data-version="v1"
+    >
+      <a
+        class="badge-base__link LI-simple-link"
+        href="https://in.linkedin.com/in/sourav-kumar-823b7415b?trk=profile-badge"
+        >
+        </a
+      >
+    </div>
+  )
+}
+
+export default App
